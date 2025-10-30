b9e69f9813251093636754b955ccb535587ee8fe
RicuxUni <902855@stud.unive.it>
2025-10-30 14:13:14 +0100

    sugamelaradio

--- app/src/main/java/com/example/prova1/FirstFragment.java
+++ app/src/main/java/com/example/prova1/FirstFragment.java
@@ -18,7 +18,7 @@ import com.google.android.gms.maps.model.LatLngBounds;
 public class FirstFragment extends Fragment {
 
     /**
-     * esndrogo
+     * esndrogo tongo
      * Questo è il "callback" che viene eseguito non appena la mappa è pronta per essere usata.
      * È qui che inseriremo tutta la logica per personalizzare la mappa.
      */
